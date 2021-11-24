979d2ca65d5bfb5acfcfe8f8a74df290cbd17649
jamie bajer <jamiebajer@hotmail.com>
2021-11-24 09:04:22 +0000

    taxi fare updates to encoders and utils

--- TaxiFareModel/encoders.py
+++ TaxiFareModel/encoders.py
@@ -1,7 +1,7 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from TaxiFareModel.utils import haversine_vectorized
+from TaxiFareModel.utils import haversine_vectorized, minkowski_distance_gps, minkowski_distance, rad2dist
 import pandas as pd
-
+import numpy as np
 
 class TimeFeaturesEncoder(BaseEstimator, TransformerMixin):
     """
@@ -46,11 +46,67 @@ class DistanceTransformer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
-        #assert isinstance(X, pd.DataFrame)
+        assert isinstance(X, pd.DataFrame)
         X_ = X.copy()
-        X_["distance"] = haversine_vectorized(X_,
-                                              start_lat=self.start_lat,
-                                              start_lon=self.start_lon,
-                                              end_lat=self.end_lat,
-                                              end_lon=self.end_lon)
-        return X_[['distance']]
+        X_["distance"] = minkowski_distance_gps(X_[self.start_lat],
+                                              X_[self.start_lon],
+                                              X_[self.end_lat],
+                                              X_[self.end_lon],1)
+        return X_
+
+
+class  DistanceToCenter(BaseEstimator, TransformerMixin):
+    """computes distance to center of NY"""
+    def __init__(self,end_lat="dropoff_latitude",
+                 end_lon="dropoff_longitude"):
+        self.end_lat = end_lat
+        self.end_lon = end_lon
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        assert isinstance(X, pd.DataFrame)
+        X_ = X.copy()
+        nyc_center = (40.7141667, -74.0063889)
+        X_["nyc_lat"], X_["nyc_lng"] = nyc_center[0], nyc_center[1]
+        args = dict(start_lat="nyc_lat",
+                    start_lon="nyc_lng",
+                    end_lat=self.end_lat,
+                    end_lon=self.end_lon)
+        X_['distance_to_center'] = haversine_vectorized(X_, **args)
+
+        return X_
+
+
+class DistanceToJFK(BaseEstimator, TransformerMixin):
+    """computes distance to center of JFK"""
+    def __init__(self,
+                 start_lat="pickup_latitude",
+                 start_lon="pickup_longitude",
+                 end_lat="dropoff_latitude",
+                 end_lon="dropoff_longitude"):
+        self.start_lat = start_lat
+        self.start_lon = start_lon
+        self.end_lat = end_lat
+        self.end_lon = end_lon
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        assert isinstance(X, pd.DataFrame)
+        X_ = X.copy()
+        jfk_center = (40.6441666667, -73.7822222222)
+
+        X_["jfk_lat"], X_["jfk_lng"] = jfk_center[0], jfk_center[1]
+        args_pickup =  dict(start_lat="jfk_lat", start_lon="jfk_lng",
+                            end_lat="pickup_latitude", end_lon="pickup_longitude")
+        args_dropoff =  dict(start_lat="jfk_lat", start_lon="jfk_lng",
+                            end_lat="dropoff_latitude", end_lon="dropoff_longitude")
+
+        X_['pickup_distance_to_jfk'] = haversine_vectorized(X_, **args_pickup)
+        X_['dropoff_distance_to_jfk'] = haversine_vectorized(
+            X_, **args_dropoff)
+
+        return X_[['pickup_distance_to_jfk','dropoff_distance_to_jfk', 'distance', 'distance_to_center']]
--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -3,15 +3,25 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from TaxiFareModel.encoders import DistanceTransformer
+from TaxiFareModel.encoders import DistanceToCenter, DistanceTransformer,DistanceToJFK
 from TaxiFareModel.encoders import TimeFeaturesEncoder
 from TaxiFareModel.utils import compute_rmse
 from TaxiFareModel.data import clean_data, get_data
+from memoized_property import memoized_property
+import mlflow
+from  mlflow.tracking import MlflowClient
+import joblib
+import xgboost as xgb
 
+
+EXPERIMENT_NAME = "[PT] [Lisbon] [J93s] Taxi_model 1.0"
+MLFLOW_URI = "https://mlflow.lewagon.co/"
 
 class Trainer():
-    def __init__(self, X_train, y_train):
+    """Please enter training data and model type"""
+    def __init__(self, X_train, y_train, **kwargs):
         """
             X: pandas DataFrame
             y: pandas Series
@@ -19,10 +29,26 @@ class Trainer():
         self.pipeline = None
         self.X_train = X_train
         self.y_train = y_train
+        self.experiment_name = EXPERIMENT_NAME
+        if "regressor" in kwargs.keys():
+            self.regressor = kwargs["regressor"]
+        else:
+            self.regressor= LinearRegression()
 
+        if self.regressor == "linear_model":
+            self.model = LinearRegression()
+        elif self.regressor == "random_forest":
+            self.model = RandomForestRegressor()
+        elif self.regressor == "XGB":
+            self.model = xgb.XGBRegressor()
+
+#,('dist_trans', DistanceTransformer())
+#('dist_JFK',DistanceToJFK()),
     def set_pipeline(self):
         '''returns a pipelined model'''
-        dist_pipe = Pipeline([('dist_trans', DistanceTransformer()),
+        dist_pipe = Pipeline([('dist_cent', DistanceToCenter()),
+                              ('dist',DistanceTransformer()),
+                                ('dist_JFK',DistanceToJFK()),
                           ('stdscaler', StandardScaler())])
         time_pipe = Pipeline([('time_enc', TimeFeaturesEncoder('pickup_datetime')),
                           ('ohe', OneHotEncoder(handle_unknown='ignore'))])
@@ -32,7 +58,9 @@ class Trainer():
         ]), ('time', time_pipe, ['pickup_datetime'])],
                                      remainder="drop")
         pipe = Pipeline([('preproc', preproc_pipe),
-                     ('linear_model', LinearRegression())])
+                     ('linear_model', self.model),
+                     ])
+        self.mlflow_log_param('model', self.regressor)
         return pipe
 
     def run(self):
@@ -46,7 +74,34 @@ class Trainer():
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
         print(rmse)
+        self.mlflow_log_metric('RMSE',rmse)
         return rmse
+
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self.pipeline, 'model.joblib')
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
@@ -60,8 +115,15 @@ if __name__ == "__main__":
     # hold out
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15)
     # train
-    trainer = Trainer(X_train, y_train)
-    trainer.run()
-    # evaluate
-    trainer.evaluate(X_test, y_test)
-    print('TODO')
+    model_list = ["linear_model", "random_forest", "XGB"]
+    for i in model_list:
+        trainer = Trainer(X_train, y_train, regressor=i)
+        print(i)
+        # trainer.mlflow_client()
+        # trainer.mlflow_experiment_id()
+        # trainer.mlflow_run()
+        trainer.run()
+        # evaluate
+        trainer.evaluate(X_test, y_test)
+        trainer.save_model()
+        print('TODO')
